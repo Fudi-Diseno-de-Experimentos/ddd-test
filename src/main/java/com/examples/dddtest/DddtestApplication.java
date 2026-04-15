840e6ba068403957f0f6439f815bf29eea2a7b9e
fabriziocpa <fabriziocpa1516@gmail.com>
2026-04-14 21:04:10 -0500

    Initial commit

---
+++ src/main/java/com/examples/dddtest/DddtestApplication.java
@@ -0,0 +1,13 @@
+package com.examples.dddtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddtestApplication.class, args);
+    }
+
+}
